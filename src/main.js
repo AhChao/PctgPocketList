d8d27b017911ed2d743a969ab86b0117b6f567da
Steven Chao <>
2024-11-09 00:17:57 +0800

    feat: refactor with more flexible way and captured sign

--- src/main.js
+++ src/main.js
@@ -7,6 +7,26 @@ const app = Vue.createApp({
                 "gen1":{
                 }
             },
+            captured:{
+                "gen1":[]
+            },
+            packs: {
+                "gen1":{
+                    "噴火龍":{
+                        color: "#B2095B",
+                        index: "0",
+                    },
+                    "超夢":{
+                        color: "#8810CA",
+                        index: "1",
+                    },
+                    "皮卡丘":{
+                        color: "#C4930B",
+                        index: "2",
+                    }
+                }
+            },
+            packCollapse:[true, true, true],
             searchQuery: "",
             oddsTable:[
                 { rarity: 'diamond1', odds: [100, 0, 0] },
@@ -23,13 +43,17 @@ const app = Vue.createApp({
     },
     mounted() {
         this.init();
+        const localCapturedData = JSON.parse(localStorage.getItem('ptcgPocketCaptured'));
+        this.captured = localCapturedData ? localCapturedData : {"gen1":[]};
     },
     computed: {
         isSmallScreen() {
             return screen.width < 568;
         },
-        filteredCards() {
-            const filteredCardsObj = JSON.parse(JSON.stringify(this.cards["gen1"]));
+    },
+    methods: {
+        filteredCards(gen) {
+            const filteredCardsObj = JSON.parse(JSON.stringify(this.cards[gen]));
             const obj = {};
             for(const pack of Object.keys(filteredCardsObj)){
                 obj[pack] = {};
@@ -41,8 +65,9 @@ const app = Vue.createApp({
             }
             return obj;
         },
-    },
-    methods: {
+        triggerCollapsePack(packId){
+            this.packCollapse[packId] = this.packCollapse[packId] ? false : true;
+        },
         init(){
             for(const cards of gen1Cards){
                 this.cards.gen1[cards["pack"]] = this.calculateOdds(this.groupByCardName(cards["card"]), cards["card"])
@@ -69,7 +94,7 @@ const app = Vue.createApp({
                     arr = [arr[0] + odds[0], arr[1] + odds[1], arr[2] + odds[2]];
                 }
             }
-            arr = arr.map(x=> x.toFixed(4).replace(/\.0+$/, "")+"%");
+            arr = arr.map(x=> x.toFixed(4).replace(/\.0+$/, ""));
             return arr;
         },
         groupByCardName(data) {
@@ -84,8 +109,46 @@ const app = Vue.createApp({
                 });
             });
             return result;
+        },
+        isCaptured(name, gen){
+            return this.captured && this.captured[gen] && this.captured[gen].includes(name) ? true : false;
+        },
+        triggerCaptured(name, gen){
+            if(this.captured[gen].includes(name)){
+                const removed = this.captured[gen].filter(x=>x != name);
+                this.captured[gen] = removed && removed.length > 0 ? removed : this.captured[gen];
+            }else{
+                this.captured[gen].push(name);
+            }
+            localStorage.setItem('ptcgPocketCaptured', JSON.stringify(this.captured));
+        },
+        getNotCapturedRate(pack, gen, pos){
+            return Object.keys(this.cards[gen][pack]).reduce((acc, card) =>{
+                    if(this.isCaptured(card, gen)) acc -= Number(this.cards[gen][pack][card][pos]);
+                    return acc;
+                }, 100).toFixed(4).replace(/\.0+$/, "");
+        },
+        copyCaptured(){
+            const capturedJSON = JSON.stringify(this.captured);
+            navigator.clipboard.writeText(capturedJSON).then(() => {
+                alert('已複製到剪貼簿，下次使用時請複製後即可會入');
+            }).catch(err => {
+                alert('複製至剪貼簿失敗');
+            });
+        },
+        overwriteCaptured(){
+            navigator.clipboard.readText().then(text => {
+                try {
+                    const capturedData = JSON.parse(text);
+                    this.captured = capturedData;
+                    alert('成功覆蓋剪貼簿的內容至目前頁面');
+                } catch (error) {
+                    alert(`剪貼簿內容不合法:${err}`);
+                }
+            }).catch(err => {
+                console.error('無法讀取剪貼簿');
+            });
         }
-        
     }
 });
 app.mount('#app');
